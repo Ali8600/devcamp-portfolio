ba890652e5444f10788fd65796b50b10eca0eb8b
Ali Idrees <aliidrees@gmail.com>
2024-04-23 14:23:54 +0500

    checking authorization

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'home/about'
   get 'home/contact'
   resources :blogs
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define your   routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
